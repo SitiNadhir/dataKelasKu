6190af9c5558b38faf03c1a178b55da0f9028236
Dira <stinadz.dir1606@gmail.com>
2024-12-28 13:56:01 +0800

    U:Update Data Kelas

--- dataKelasKu.cpp
+++ dataKelasKu.cpp
@@ -23,7 +23,7 @@ void tampilkanMenu() {
     cout << "==============================" << endl;
     cout << "1. Masukkan Data" << endl;
     cout << "2. Tampilkan  Data Kelas" << endl;
-    cout << "3. Update Data" << endl;
+    cout << "3. Update Data Kelas" << endl;
     cout << "4. Hapus Data" << endl;
     cout << "5. Keluar" << endl;
     cout << "==============================" << endl;
